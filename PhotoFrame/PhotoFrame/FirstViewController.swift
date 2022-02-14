e46aad398c738a6c44e8f71730ad8225f5af2218
JinSeob <akdlsjtm21@naver.com>
2022-02-14 21:33:13 +0900

    add FirstLabel

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class FirstViewController:UIViewController {
     
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
-        
+        configureLabel()
+    }
+    
+    func configureLabel() {
+        self.photoLabel.text = "피그백's 사진액자"
+        self.photoLabel.textColor = .red //색깔
+        self.photoLabel.textAlignment = .center //정렬
+        self.photoLabel.font = .systemFont(ofSize: 40.0, weight: .bold) //사이즈,폰트
     }
     
 }
