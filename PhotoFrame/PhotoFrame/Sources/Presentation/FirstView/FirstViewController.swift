f831e9dde673801df9ae8a20ace4c7a89f9bcd5f
shingha <shingha1124@gmail.com>
2022-02-14 20:34:45 +0900

    [PF-2][feature] IBOutlet, IBAction 연결하기

--- PhotoFrame/PhotoFrame/Sources/Presentation/FirstView/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/FirstView/FirstViewController.swift
@@ -9,10 +9,20 @@ import Foundation
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(#file, #line, #function, #column)
+        firstLabel.text = "Shingha의 사진액자"
+    }
+    
+    @IBAction func onTappedButton(_ sender: Any) {
+        self.firstLabel.textColor = UIColor.blue
+        self.firstLabel.backgroundColor = UIColor.yellow
+        self.firstLabel.alpha = 0.5
     }
 }
