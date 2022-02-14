f926bdeb6003946c3868a4b77075efd7dffdb850
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-02-14 11:09:44 +0900

    Add and Commit Project Folder
    프로젝트 폴더를 Add/Commit 합니다.
    두 작업을 동시에 하자는 의미와 수업/작업 중엔 웬만하면
    IDE 화면만 보자는 생각이 있어서 Git 관련 작업은
    Xcode를 이용할 예정입니다.

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by 백상휘 on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoFrame/PhotoFrameUITests/PhotoFrameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoFrameUITestsLaunchTests.swift
+//  PhotoFrameUITests
+//
+//  Created by 백상휘 on 2022/02/14.
+//
+
+import XCTest
+
+class PhotoFrameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
