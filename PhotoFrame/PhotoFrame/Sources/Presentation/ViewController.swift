d02abfc23cfe0c950a447d7fbd85da088b8982ef
shingha <shingha1124@gmail.com>
2022-02-14 10:36:02 +0900

    [PF-1] 프로젝트 생성 및 폴더 정리

---
+++ PhotoFrame/PhotoFrame/Sources/Presentation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by seongha shin on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
