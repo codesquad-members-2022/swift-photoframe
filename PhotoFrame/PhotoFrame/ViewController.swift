bd0892b34aea63686ffebc076b3e30887589a833
sol <hskim6521@naver.com>
2022-02-14 22:17:19 +0900

    Feat: First Scene에 UILabel 추가하여 IBOutlet 변수(photoLabel)로 설정

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
