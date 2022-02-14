2fe900fa8bac77430909c9d8d75916ea1b4dc4c2
Damagucci-Juice <jsr9874@gmail.com>
2022-02-14 11:44:44 +0900

    Add PhotoFrame Project

---
+++ PhotoFrame/PhotoFrame/PhotoFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFrameApp.swift
+//  PhotoFrame
+//
+//  Created by YEONGJIN JANG on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
