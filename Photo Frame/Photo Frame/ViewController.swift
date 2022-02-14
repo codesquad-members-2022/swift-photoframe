e5d45395fed686e5fdfb6805f20bd5b4c304e8cf
Jihee hwang <hwangjihee94@gmail.com>
2022-02-14 11:33:15 +0900

    Test

---
+++ Photo Frame/Photo Frame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Frame
+//
+//  Created by Jihee hwang on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Photo Frame/Photo FrameUITests/Photo_FrameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Photo_FrameUITestsLaunchTests.swift
+//  Photo FrameUITests
+//
+//  Created by Jihee hwang on 2022/02/14.
+//
+
+import XCTest
+
+class Photo_FrameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
