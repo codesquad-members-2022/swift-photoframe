6282d5a975d164c9c28aa0f826b6787c889dace9
Lee SeEun <sally4405@naver.com>
2022-02-14 14:42:23 +0900

    [step2] feat: first label에 IBOulet 추가하고 속성 변경

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -2,8 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstLabel.text = "sally의 사진액자"
         
     }
 
