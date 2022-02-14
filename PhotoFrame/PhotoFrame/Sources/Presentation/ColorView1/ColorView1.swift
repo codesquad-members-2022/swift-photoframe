ed4487e240c8f1233c4eedc0b863aec7605d9ab8
shingha <shingha1124@gmail.com>
2022-02-14 22:20:48 +0900

    [PF-3][feature] ViewController 연결하기

---
+++ PhotoFrame/PhotoFrame/Sources/Presentation/ColorView1/ColorView1.swift
@@ -0,0 +1,35 @@
+//
+//  ColorView1.swift
+//  PhotoFrame
+//
+//  Created by seongha shin on 2022/02/14.
+//
+
+import UIKit
+
+class ColorView1: UIViewController {
+    
+    override func viewDidLoad() {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
