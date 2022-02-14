7d7d106ac2b251a7e0687b590de46b0700ab0007
shingha <shingha1124@gmail.com>
2022-02-14 12:10:29 +0900

    [PF-1] README 정리
    README를 정리하면서 약간 수정된 파일들도 같이 커밋

--- PhotoFrame/PhotoFrame/Sources/Presentation/Home/TabBarController.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/Home/TabBarController.swift
@@ -12,6 +12,5 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("TabBarController")
     }
 }
