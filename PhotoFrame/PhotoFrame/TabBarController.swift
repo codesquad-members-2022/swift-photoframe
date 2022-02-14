499e23320ab7165bdb976077d907fac9b5c6f64e
Damagucci-Juice <jsr9874@gmail.com>
2022-02-14 21:27:09 +0900

    Update README.md

---
+++ PhotoFrame/PhotoFrame/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  PhotoFrame
+//
+//  Created by YEONGJIN JANG on 2022/02/14.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+        print("Hello World")
+    }
+}
+
+
+
---
+++ PhotoFrame/PhotoFrame/ViewController2.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController2.swift
+//  PhotoFrame
+//
+//  Created by YEONGJIN JANG on 2022/02/14.
+//
+
+import Foundation
+import UIKit
+
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
+        print("Hello World cont2")
+    }
+}
