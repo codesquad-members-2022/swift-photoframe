474324699864e9bb0b7460a2ebf38d550d1da07a
Damagucci-Juice <jsr9874@gmail.com>
2022-02-14 23:32:24 +0900

    Fix Label's detail settings

--- PhotoFrame/PhotoFrame/ViewController1.swift
+++ PhotoFrame/PhotoFrame/ViewController1.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController1 : UIViewController {
  
+    @IBOutlet weak var photoLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(#file, #line, #function, #column)
         print("Hello World cont1")
-
+        self.photoLabel.text = "Damagucci-Juice의 포토 프레임"
+        self.photoLabel.font = UIFont.systemFont(ofSize: CGFloat(15))
+        self.photoLabel.textColor = UIColor.blue
+        self.photoLabel.backgroundColor = UIColor.darkGray.withAlphaComponent(0.5)
     }
 }
