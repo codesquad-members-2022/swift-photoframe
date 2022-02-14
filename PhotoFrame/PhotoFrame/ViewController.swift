c6700a28578873a4ee0490f7c261670479757cee
sungju-kim <wnwn0719@gmail.com>
2022-02-14 11:10:06 +0900

    add tabBar Controller

---
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by dale on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
