4dd2b61d913681123ee2bcf74df320d1a5653c43
Lee SeEun <sally4405@naver.com>
2022-02-14 15:25:49 +0900

    [step2] feat: first description에 IBOulet 추가하고 속성 변경

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -3,11 +3,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstDesciption: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.firstLabel.text = "sally의 사진액자"
         
+        self.firstLabel.text = "sally의 사진액자"
+        self.firstLabel.backgroundColor = .lightGray
+        self.firstLabel.layer.borderColor = .init(gray: 0.3, alpha: 1)
+        self.firstLabel.layer.borderWidth = 3
+        self.firstLabel.layer.cornerRadius = 5
+        
+        self.firstDesciption.text = "첫번째 화면입니다."
+        self.firstDesciption.textColor = .darkGray
     }
 
 
