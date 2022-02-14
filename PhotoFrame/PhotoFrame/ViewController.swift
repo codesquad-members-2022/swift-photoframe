d1417ddde4e13439a015b855fb3fb6e07f3f330d
Jason <ggj00125@gmail.com>
2022-02-14 21:44:16 +0900

    [Update] ReadMe.md 업데이트

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(#file, #line, #function, #column)
     }
-
-
+    
 }
 
