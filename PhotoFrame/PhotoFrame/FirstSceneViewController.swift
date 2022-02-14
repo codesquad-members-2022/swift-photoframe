dc9dd76b97bad805a6e462e1dbe98f85bf456a2e
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-02-14 20:15:40 +0900

    Convert InitialViewController as UITabBarController
    isInitialViewController 속성에 대해 실습하고,
    print 가능한 여러 속성에 대해 실습한 뒤 커밋합니다.
    추가학습거리인 View Management Class에 대해 학습할 예정입니다.

---
+++ PhotoFrame/PhotoFrame/FirstSceneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstSceneViewController.swift
+//  PhotoFrame
+//
+//  Created by 백상휘 on 2022/02/14.
+//
+
+import UIKit
+
+class FirstSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
