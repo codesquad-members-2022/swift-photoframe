f0bf94ddca99b14c1d0b10761ddd3664429c992c
asqw887 <asqw887@gmail.com>
2022-02-15 16:42:13 +0900

    PhotoLabel 속성 추가

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -14,12 +14,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureLabel()
 //        print(#file, #line, #function, #column)
         
 
         // Do any additional setup after loading the view.
     }
     
+    
+    func configureLabel() {
+        
+        photoLabel.text = "Chez의 사진액자"
+        photoLabel.textColor = .blue
+        photoLabel.backgroundColor = .yellow
+        photoLabel.alpha = 0.5
+        photoLabel.font = UIFont.systemFont(ofSize: 50)
+        
+        photoLabel.sizeToFit()  // 사이즈를 텍스트에 맞게 조정
+        photoLabel.center.x = self.view.frame.width/2
+        
+        
+        
+        
+        
+        
+        
+    }
 
     /*
     // MARK: - Navigation
