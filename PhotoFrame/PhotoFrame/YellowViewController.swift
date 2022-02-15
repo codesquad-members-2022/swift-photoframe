267feef3517ee544fb6ea6227300060dfd13004a
sol <hskim6521@naver.com>
2022-02-16 01:13:08 +0900

    Fix: [다음] 버튼의 기능을 새로 정의
    이전 PR에서 질문받아서 공부했던, Segue없이 Modal을 이용한 화면 전환방법을 이용하기 위해 [다음] 버튼의 기능을 새로 정의
    [다음] 버튼을 누르면 identifier가 "blueModal"인 화면으로 전환되도록 재설정

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -13,6 +13,12 @@ class YellowViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    @IBAction func nextButtonTouched(_ sender: UIButton) {
+        let blueModalVC = self.storyboard?.instantiateViewController(withIdentifier: "blueModal")
+        blueModalVC?.modalTransitionStyle = .flipHorizontal
+        blueModalVC?.modalPresentationStyle = .automatic
+        self.present(blueModalVC!, animated: true, completion: nil)
+    }
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
