fdef2cc5af10cca7fe83ff0d7a5f8aafad55a3c3
asqw887 <asqw887@gmail.com>
2022-02-15 15:17:44 +0900

    label추가, IBOutlet 연결

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var photoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(#file, #line, #function, #column)
+//        print(#file, #line, #function, #column)
+        
 
         // Do any additional setup after loading the view.
     }
