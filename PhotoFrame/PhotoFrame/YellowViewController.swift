676d29be6b69784b4325a5aa842c3295eb730850
sol <hskim6521@naver.com>
2022-02-16 00:42:14 +0900

    Feat: YellowViewController 클래스 생성하여 ViewController의 Custom class로 지정

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,16 @@
+//
+//  YellowViewController.swift
+//  PhotoFrame
+//
+//  Created by 김한솔 on 2022/02/16.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
