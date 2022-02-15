03fb1f457ede65660ea67b8c8cb8e265cd1f1435
sungju-kim <wnwn0719@gmail.com>
2022-02-15 18:52:32 +0900

    EDIT UIViewController를 상속받는 CallBackMethod class 를 만들고, ViewController, GreyViewController, TintViewController class의 부모로  CallBackMethod 를 상속받음

--- PhotoFrame/PhotoFrame/TintViewController.swift
+++ PhotoFrame/PhotoFrame/TintViewController.swift
@@ -7,28 +7,8 @@
 
 import UIKit
 
-class TintViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+class TintViewController: CallBackMethod {
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print(#file, #line, #function, #column)
-    }
-    override  func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        print(#file, #line, #function, #column)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        print(#file, #line, #function, #column)
-    }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(true)
-        print(#file, #line, #function, #column)
     }
 }
