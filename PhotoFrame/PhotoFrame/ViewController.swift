d634936e29a4d9d4417e0b09ddbd2d7a8c84f786
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-15 13:54:38 +0900

    UILabel 추가 후 IBOutlet 연결
    1. 첫번째 scene 내부에 UILabel 추가
    2. ViewController 클래스에 IBOutlet 연결
    3. 코드를 통해 text 프로퍼티에 값을 할당하여 나타나게 될 문자열 변경

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -2,9 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
+        firstLabel.text = "Jed의 사진 액자"
     }
 }
 
