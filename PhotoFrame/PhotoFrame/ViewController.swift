acf98d03227a5cbcc1e9a97277c3fe1ee4a8a3f7
BumgeunSong <isp1195@gmail.com>
2022-02-15 21:38:18 +0900

    Change label: text/fontSize/color/bgColor

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        photoLabel.text = "Eddy의 사진 액자"
+        photoLabel.font = .systemFont(ofSize: CGFloat(40))
+        photoLabel.textColor = UIColor(
+            red: CGFloat(46)/255,
+            green: CGFloat(148)/255,
+            blue: CGFloat(185)/255,
+            alpha: 1)
+        photoLabel.backgroundColor = UIColor(
+            red: CGFloat(255)/255,
+            green: CGFloat(253)/255,
+            blue: CGFloat(193)/255,
+            alpha: 0.5)
     }
 
 
