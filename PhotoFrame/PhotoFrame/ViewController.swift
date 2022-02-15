fa29481812bc2e6a8795b2519132db548afcbd45
BumgeunSong <isp1195@gmail.com>
2022-02-15 21:24:39 +0900

    Create photolabel as IBOutlet

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
