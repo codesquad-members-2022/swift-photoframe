9564cdd940e0a1ed0b1e7d69a4710a9e29de5592
sanghee <anshee97@naver.com>
2022-02-16 03:54:51 +0900

    [Add] Add UIButton programmatically

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  PhotoFrame
 //
-//  Created by 안상희 on 2022/02/14.
+//  Created by Selina on 2022/02/14.
 //
 
 import UIKit
@@ -12,24 +12,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet weak var firstDescription: UILabel!
     
+    let firstButton = UIButton(type: UIButton.ButtonType.system) as UIButton
+    
+    @objc func buttonAction(_ sender:UIButton!) {
+        print("Button tapped")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        firstButton.frame = CGRect(x: self.view.center.x, y: 150, width: 150, height: 45)
+        firstButton.backgroundColor = UIColor.systemPink
+        firstButton.setTitle("Tap me!", for: UIControl.State.normal)
+        firstButton.tintColor = .white
+        firstButton.addTarget(self, action: #selector(self.buttonAction), for: .touchUpInside)
+        
+        
+        
         // subView 추가
+        self.view.addSubview(firstButton)
         self.view.addSubview(firstLabel)
         self.view.addSubview(firstDescription)
         
         // Constraints를 코드로 작성하기 위함.
+//        self.firstButton.translatesAutoresizingMaskIntoConstraints = false
         self.firstLabel.translatesAutoresizingMaskIntoConstraints = false
         self.firstDescription.translatesAutoresizingMaskIntoConstraints = false
         
+        
+        
         // firstLabel 속성
         self.firstLabel.text = "Selina의 사진액자"
-        
         self.firstLabel.textColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
-        
-        
-        
         
         self.firstLabel.backgroundColor = UIColor(red: 250 / 255.0, green: 197 / 255.0, blue: 210 / 255.0, alpha: 1)
         self.firstLabel.backgroundColor = UIColor(cgColor: CGColor(red: 250 / 255.0, green: 197 / 255.0, blue: 210 / 255.0, alpha: 1))
