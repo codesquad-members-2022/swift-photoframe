2e642ff085585cbada26ed1b957db8f6b86acbe2
Mase <57667738+sanghyeok-kim@users.noreply.github.com>
2022-02-15 11:16:25 +0900

    [Mase] Step2 - IBOutlet 연결 및 구현 (#24)
    * [Add] 포토프레임 프로젝트 생성
    
    * [Implement] 1. 프로젝트 생성하기 구현
    
    * [Docs] *.xcuserstate 추가
    
    * [Add] photoLabel, firstDescription 생성 및 @IBOutlet을 통해 연결
    
    * [Implement] photoLabel 속성 및 AutoLayout 구현
    
    * [Implement] firstDescription 속성 및 AutoLayout 구현
    
    * [Docs] Step2 Readme 작성 완료

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    @IBOutlet weak var firstDescription: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemTeal
         
-        print(#file, #line, #function, #column)
+        photoLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstDescription.translatesAutoresizingMaskIntoConstraints = false
+        
+        photoLabel.text = "Mase의 사진액자"
+        photoLabel.textColor = .systemBlue
+        photoLabel.font = photoLabel.font.withSize(CGFloat(30.0))
+        photoLabel.textAlignment = .center
+        photoLabel.backgroundColor = .systemGray6
+        photoLabel.alpha = 0.9
+        
+        photoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        photoLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        
+        firstDescription.text = "첫 번째 Scene"
+        firstDescription.textColor = .systemBlue
+        firstDescription.font = firstDescription.font.withSize(CGFloat(20.0))
+        firstDescription.textAlignment = .center
+        firstDescription.backgroundColor = .systemGray6
+        firstDescription.alpha = 0.7
+        
+        firstDescription.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        firstDescription.centerYAnchor.constraint(equalTo: photoLabel.bottomAnchor, constant: 50).isActive = true
     }
-
-
 }
-
