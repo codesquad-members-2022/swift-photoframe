fe45965c0ae2636e69d9bdddaa359504a6cc83bb
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-15 14:06:28 +0900

    UILabel 속성 변경하기
    1. NSMutableAttributedString을 사용해서 텍스트 속성 생성
    2. addAttributes를 통해 글자 음영 및 사이즈 변경

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -7,7 +7,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
-        firstLabel.text = "Jed의 사진 액자"
+        
+        //라벨에 보여질 텍스트, 속성 설정
+        let attributedText = NSMutableAttributedString(string: "Jed의 사진 액자")
+        attributedText.addAttributes([.backgroundColor: UIColor.darkGray, .font: UIFont.systemFont(ofSize: 40)], range: NSRange(location: 0, length: 10))
+        firstLabel.attributedText = attributedText
     }
 }
 
