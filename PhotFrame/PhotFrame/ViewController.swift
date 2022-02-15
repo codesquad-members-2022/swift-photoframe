adb84f76748a3df5598397e5222b2c626fd82912
Joobang <84652513+LeeJoobang@users.noreply.github.com>
2022-02-15 19:01:18 +0900

    [joobang] Step004 - IBOutlet, IBAction, Segue 작업 수행 (#49)
    * 1. 사진액자 앱
     - 브랜치(day1_mon) 생성
     - 테스트 겸 커밋
    
    * 1. ViewController 클래스 viewDidLoad() 함수 내 값 입력
     - print(#file, #line, #function, #column)
    
    * 1. README.md 파일 초안 작성 커밋
    
    * 1. rebase 수행 전 작업분에 대한 업로드(iBoutlet 진행중이었음)
    
    * 1. IBOutlet 연결하기
     - joobang의 사진액자 텍스트입력
     - Loaded by First ViewController 텍스트 입력
    2. IBAction 연결하기
     - '다음'버튼 입력
     - 버튼 클릭 후 텍스트 상태 변화
    3. Scene -> Segue 연결
     - 버튼 클릭 후 3단계에 걸쳐 바탕 화면 변화(첫화면 -> 주황색 -> 빨간색)

--- PhotFrame/PhotFrame/ViewController.swift
+++ PhotFrame/PhotFrame/ViewController.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet var firstDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstLabel.text = "joobang의 사진액자"
         print(#file, #line, #function, #column)
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func nextButtonTouched(_ sender: Any) {
+        self.firstLabel.textColor = UIColor.blue
+            self.firstLabel.backgroundColor = UIColor.yellow
+            self.firstLabel.alpha = 0.5
+    }
+    
 }
 
