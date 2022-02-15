94e87cd915c355995a866c794ad719ce816b95cc
kimdongjune <jun36817@naver.com>
2022-02-15 17:34:57 +0900

    YellowViewController에 생명주기를 추가하고, 이에 흐름을 파악해보았습니다.

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -11,10 +11,46 @@ import UIKit
 class YellowViewController: UIViewController{
     
     @IBOutlet weak var closeBtn: UIButton!
+    private var greenVC: UIViewController?
+    
+    override func loadView() {
+        super.loadView()
+        print("\(self.classForKeyedArchiver!) loadView")
+        self.greenVC =  self.storyboard?.instantiateViewController(withIdentifier: "GreenVC")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\(self.classForKeyedArchiver!) viewDidLoad")
         setViewSetting(color: .yellow,btn: self.closeBtn)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("\(self.classForKeyedArchiver!) viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("\(self.classForKeyedArchiver!) viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("\(self.classForKeyedArchiver!) viewWillDisAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("\(self.classForKeyedArchiver!) viewDidDisAppear")
+    }
+    
+    @IBAction func openButtonTouched(_ sender: Any) {
+        guard let greenVC = self.greenVC else {
+            return
+        }
+
+        self.present(greenVC, animated: true, completion: nil)
     }
     
     @IBAction func closeButtonTouched(_ sender: Any) {
