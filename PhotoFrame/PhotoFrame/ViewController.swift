73b08785a27d4429d316ab40037090858fed5e6a
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-15 18:13:31 +0900

    firstLabel 텍스트 속성 일부 수정
    - 백그라운드 색상을 라벨 전체로 적용
    - 라벨의 위치를 화면 정중앙으로 적용

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -10,8 +10,14 @@ class ViewController: UIViewController {
         
         //라벨에 보여질 텍스트, 속성 설정
         let attributedText = NSMutableAttributedString(string: "Jed의 사진 액자")
-        attributedText.addAttributes([.backgroundColor: UIColor.darkGray, .font: UIFont.systemFont(ofSize: 40)], range: NSRange(location: 0, length: 10))
+        attributedText.addAttributes([.foregroundColor: UIColor.white,
+                                      .font: UIFont.systemFont(ofSize: 40)]
+                                     , range: NSRange(location: 0, length: 10))
         firstLabel.attributedText = attributedText
+        firstLabel.backgroundColor = UIColor.lightGray
+        //가운데 정렬
+        firstLabel.center.x = self.view.center.x
+        firstLabel.center.y = self.view.center.y
     }
 }
 
