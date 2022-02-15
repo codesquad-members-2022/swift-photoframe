277172ddfd4b0e9b83484e43d87a7ea364d80be3
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-16 00:07:29 +0900

    테스트용 버튼 3개 추가 및 가운데 정렬

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -3,7 +3,12 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
     
+    @IBOutlet weak var testButton1: UIButton!
+    @IBOutlet weak var testButton2: UIButton!
+    @IBOutlet weak var testButton3: UIButton!
+
     @IBAction func nextButtonTouchedUpInside(_ sender: Any) {
         self.firstLabel.textColor = UIColor.blue
         self.firstLabel.backgroundColor = UIColor.yellow
@@ -36,6 +41,11 @@ class ViewController: UIViewController {
         //가운데 정렬
         firstLabel.center.x = self.view.center.x
         firstLabel.center.y = self.view.center.y
+        
+        nextButton.center.x = self.view.center.x
+        testButton1.center.x = self.view.center.x/2
+        testButton2.center.x = self.view.center.x
+        testButton3.center.x = self.view.center.x*1.5
     }
 }
 
