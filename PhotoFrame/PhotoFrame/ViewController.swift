4265822b3530e80bc06924efab33331e56c7ebca
BumgeunSong <isp1195@gmail.com>
2022-02-15 14:51:34 +0900

    Add Print: Literal Expression

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#file)
+        print(#line, #column)
+        print(#function)
+        
     }
 
 
