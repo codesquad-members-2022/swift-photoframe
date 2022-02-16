ccbcd6b06d6ba8d9e26213673a6fffd5e6604aab
Song TaeHwan <axoghks@gmail.com>
2022-02-16 12:55:37 +0900

    feat: Add DesignableUILabel and DesignableUIButton classes

---
+++ PhotoFrame/PhotoFrame/Components/CustomView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomView.swift
+//  PhotoFrame
+//
+//  Created by 송태환 on 2022/02/16.
+//
+
+import UIKit
+
+@IBDesignable
+class CustomView: UIView {
+    
+}
---
+++ PhotoFrame/PhotoFrame/Components/DeginableUILabel.swift
@@ -0,0 +1,48 @@
+//
+//  DesignableUILabel.swift
+//  PhotoFrame
+//
+//  Created by 송태환 on 2022/02/16.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableUILabel: UILabel {
+    @IBInspectable var paddingTop: CGFloat = 0
+    @IBInspectable var paddingBottom: CGFloat = 0
+    @IBInspectable var paddingLeft: CGFloat = 0
+    @IBInspectable var paddingRight: CGFloat = 0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: paddingTop, left: paddingLeft, bottom: paddingBottom, right: paddingRight) // 패딩
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize // 컨텐츠 크기
+        return CGSize(width: size.width + paddingLeft + paddingRight, height: size.height + paddingTop + paddingBottom) // 컨텐츠 크기 + 패딩
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor = .black {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+            self.layer.masksToBounds = cornerRadius > 0
+        }
+    }
+}
