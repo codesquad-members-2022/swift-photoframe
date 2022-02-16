3212e5315f3ee4c7038dabc6c5cdf5b6162ab4c8
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-17 02:04:56 +0900

    [Feat] BlueViewController의 closeButton 구현 완료

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class BlueViewController: UIViewController {
     
+    @IBOutlet weak var closeButton: UIButton!
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        closeButton.setTitle("닫기", for: .normal)
+        closeButton.backgroundColor = .lightGray
+        closeButton.layer.cornerRadius = 10
+        
+        closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
     }
 }
