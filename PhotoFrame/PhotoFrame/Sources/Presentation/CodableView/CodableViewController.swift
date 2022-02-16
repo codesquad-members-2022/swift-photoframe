3af26a3415fc333e3f37a032d844a2ca66949b01
shingha <shingha1124@gmail.com>
2022-02-16 09:21:55 +0900

    [add] 코드로 만들 Scene에 사용할 클래스 추가 및 연결

---
+++ PhotoFrame/PhotoFrame/Sources/Presentation/CodableView/CodableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CodableView.swift
+//  PhotoFrame
+//
+//  Created by seongha shin on 2022/02/16.
+//
+
+import Foundation
+import UIKit
+
+class CodableViewController: UIViewController {
+    
+}
