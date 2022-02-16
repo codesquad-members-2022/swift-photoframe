f9809644b35d4759a03eaaca0eccfdae937fccdc
shingha <shingha1124@gmail.com>
2022-02-16 21:46:34 +0900

    [study] 화면 전환이 이루어지는 사이에 뷰컨트롤러 라이프사이클이 어떻게 변화하는지 학습한다.

--- PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/StudyViewController.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/StudyViewController.swift
@@ -8,50 +8,10 @@
 import Foundation
 import UIKit
 
-enum SegueType {
-    case show, showDetail, presentModally, presentAsPopover
-}
-
-class CustomButton: UIButton {
-    var segueType: SegueType = .show
-}
-
 class StudyViewController: UIViewController {
-    
-    @IBOutlet weak var show: CustomButton!
-    @IBOutlet weak var showDetail: CustomButton!
-    @IBOutlet weak var presentModally: CustomButton!
-    @IBOutlet weak var presentAsPopover: CustomButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        show.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
-        show.segueType = .show
-        showDetail.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
-        showDetail.segueType = .showDetail
-        presentModally.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
-        presentModally.segueType = .presentModally
-        presentAsPopover.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
-        presentAsPopover.segueType = .presentAsPopover
-    }
-    
-    @objc
-    func onTestButtonTapped(_ sender: CustomButton) {
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let nextVC = mainStoryboard.instantiateViewController(withIdentifier: "NextViewController")
-        
-        switch sender.segueType {
-        case .show:
-            self.show(nextVC, sender: nil)
-        case .showDetail:
-            self.showDetailViewController(nextVC, sender: nil)
-        case .presentModally:
-            self.present(nextVC, animated: true, completion: nil)
-        case .presentAsPopover:
-            self.isModalInPopover = true
-            self.present(nextVC, animated: true, completion: nil)
-        }
     }
 }
 
