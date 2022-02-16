bb2686b01e54d86bb308ad0647ed37792ad87897
Dihee <92635121+Dihee@users.noreply.github.com>
2022-02-16 11:16:42 +0900

    [Jee] Step2 - IBOutlet 연결하기 / Step3 - IBAction 연결하기 (#50)
    * Test
    
    * Update README.md
    
    * no message
    
    * Complete step 2 / 3
    
    * Update README.md
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- Photo Frame/Photo Frame/ViewController.swift
+++ Photo Frame/Photo Frame/ViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoLabel: UILabel!
+    @IBOutlet weak var firstDescription: UILabel!
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.photoLabel.text = "Jihee's Photo"
+        self.photoLabel.textColor = .darkGray
+        self.firstDescription.text = "Jihee's Photo Frame"
+        self.firstDescription.textColor = .white
+        self.firstDescription.backgroundColor = .gray
+        
+    
         print(#file, #line, #function, #column)
     }
     
-
+    @IBAction func nextButtonTouched(_ sender: UIButton) {
+        self.photoLabel.text = "Change the color"
+        self.photoLabel.font = UIFont(name: "Futura Bold", size: 33)
+        self.photoLabel.textColor = .tintColor
+    }
+    
     /*
     // MARK: - Navigation
 
