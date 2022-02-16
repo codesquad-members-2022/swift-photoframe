de00069832e4e1962fab5833812355fb8bdb9aae
sol <hskim6521@naver.com>
2022-02-16 12:42:05 +0900

    Feat: 모든 Scene에 해당 View가 몇 번째 View인지 나타내는 UILabel 추가
    UILabel의 attributedText 프로퍼티를 이용해, Label의 Text 일부를 원하는 형태로 수정해보았습니다.

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class YellowViewController: UIViewController {
 
+    @IBOutlet weak var secondViewIndicatorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        secondViewIndicatorLabel.text = "Second View"
+        secondViewIndicatorLabel.font = .systemFont(ofSize: 25)
+        
+        let fontSize = UIFont.boldSystemFont(ofSize: 35)
+        let attributedString = NSMutableAttributedString(string: secondViewIndicatorLabel.text!)
+        attributedString.addAttribute(.font, value: fontSize, range: (secondViewIndicatorLabel.text! as NSString).range(of: "Second"))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.systemRed, range: (secondViewIndicatorLabel.text! as NSString).range(of: "Second"))
+        secondViewIndicatorLabel.attributedText = attributedString
         print(#file, #line, #function, #column)
     }
     
