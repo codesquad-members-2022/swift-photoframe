0eacbd4cce0b09662f3a9a6bc4239db5a56331e1
shingha <shingha1124@gmail.com>
2022-02-16 12:55:25 +0900

    [Design] 사진액자 디자인 구현

--- PhotoFrame/PhotoFrame/Sources/Presentation/CodableView/CodableViewController.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/CodableView/CodableViewController.swift
@@ -10,4 +10,31 @@ import UIKit
 
 class CodableViewController: UIViewController {
     
+    let photoBoardImageView = UIImageView()
+    let photoImageView = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        attributes()
+        layout()
+    }
+    
+    func attributes() {
+        photoBoardImageView.image = UIImage(named: "photoframe-border.jpg")
+        photoImageView.image = getImage()
+    }
+    
+    func layout() {
+        self.view.addSubViews([photoBoardImageView, photoImageView])
+        
+        photoBoardImageView.frame = CGRect(x: 0, y: 0, width: 270, height: 270)
+        photoBoardImageView.center = self.view.center
+        
+        photoImageView.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
+        photoImageView.center = self.view.center
+    }
+    
+    private func getImage() -> UIImage? {
+        return UIImage(named: String(format: "%02d.jpg", Int.random(in: 1...22)))
+    }
 }
