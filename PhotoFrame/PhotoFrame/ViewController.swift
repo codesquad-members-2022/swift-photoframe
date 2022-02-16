ee95e10c8f7a7c8d81f1872d9be31b9a6b4931d9
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-16 12:08:27 +0900

    테스트 버튼 제거
    - 스토리보드에서 테스트 버튼 제거
    - ViewController 클래스 내에서 해당 버튼을 참조하던 프로퍼티 변수 및 액션 함수 모두 제거

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -4,10 +4,6 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
-    
-    @IBOutlet weak var testButton1: UIButton!
-    @IBOutlet weak var testButton2: UIButton!
-    @IBOutlet weak var testButton3: UIButton!
 
     @IBAction func nextButtonTouchedUpInside(_ sender: Any) {
         self.firstLabel.textColor = UIColor.blue
@@ -16,11 +12,7 @@ class ViewController: UIViewController {
         
         print("touched up inside")
     }
-  
-    @IBAction func testButtonTouchedUpInside(_ sender: UIButton) {
-        print("\(sender.hash) : test button touched up inside")
-    }
-    
+
     @IBAction func nextButtonTouchedUpOutside(_ sender: Any) {
         self.firstLabel.textColor = UIColor.red
         print("touched up outside")
@@ -47,9 +39,6 @@ class ViewController: UIViewController {
         firstLabel.center.y = self.view.center.y
         
         nextButton.center.x = self.view.center.x
-        testButton1.center.x = self.view.center.x/2
-        testButton2.center.x = self.view.center.x
-        testButton3.center.x = self.view.center.x*1.5
     }
 }
 
