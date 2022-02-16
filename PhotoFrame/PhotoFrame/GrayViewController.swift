8efc7f00d1bec19ee8113ed7c8931328c8eed973
BumgeunSong <isp1195@gmail.com>
2022-02-16 21:18:34 +0900

    Add button that shows yellowViewController

--- PhotoFrame/PhotoFrame/GrayViewController.swift
+++ PhotoFrame/PhotoFrame/GrayViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class GrayViewController: UIViewController {
-
+    @IBAction func showButtonTouched(_ sender: UIButton) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let yellowViewController = storyBoard.instantiateViewController(withIdentifier: "YellowViewController")
+        show(yellowViewController, sender: self)
+    }
+    
+    
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -39,15 +45,15 @@ class GrayViewController: UIViewController {
         print(#file, #line, #function, #column)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
