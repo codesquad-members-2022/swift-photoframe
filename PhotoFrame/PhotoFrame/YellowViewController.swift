da682276cb31f57af25686dc22573a7cc78232c8
BumgeunSong <isp1195@gmail.com>
2022-02-16 20:10:52 +0900

    Add close Button to Yellow ViewController

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class YellowViewController: UIViewController {
 
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
 
