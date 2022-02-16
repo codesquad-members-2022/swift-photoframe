ebb0b481e55a55a15d69c866125d4f3050199c97
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-17 02:00:52 +0900

    [Add] BlueViewController 생성

---
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BlueViewController.swift
+//  PhotoFrame
+//
+//  Created by 김상혁 on 2022/02/17.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
