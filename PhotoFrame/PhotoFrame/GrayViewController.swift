acd254bcbcb1d4b876fa791252a701820756c647
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-16 16:43:48 +0900

    ViewController 클래스 추가 후 라이프사이클 확인
    - viewWillAppear, viewDidAppear, viewWillDisappear, viewDidDisappear 각각 오버라이딩해서 어느 시점에 함수가 호출되는 지 확인
    - 닫기버튼 만들어서 화면 닫는 액션 구현

---
+++ PhotoFrame/PhotoFrame/GrayViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class GrayViewController: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var exitButton: UIButton!
+    
+    @IBAction func exitButtonTouched(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setButtonPosition()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    func setButtonPosition(){
+        nextButton.center.x = self.view.center.x
+        nextButton.center.y = self.view.center.y
+        exitButton.center.x = self.view.center.x
+        exitButton.center.y = self.view.center.y*0.5
+    }
+}
