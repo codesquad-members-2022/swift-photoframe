d5aad1250c36625a20b85501858704f5a91d25e1
asqw887 <asqw887@gmail.com>
2022-02-16 16:26:38 +0900

    SecondViewController 생성 및 배경색 설정

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by 최예주 on 2022/02/16.
+//
+
+import UIKit
+
+class SecondViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
