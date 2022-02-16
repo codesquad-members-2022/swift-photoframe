4f08e6baf10189e788ed6a5534c5dd0404fc0fc9
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-17 01:59:42 +0900

    [Feat] YellowViewController의 closeButton 구현 완료

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class YellowViewController: UIViewController {
 
-
+    @IBOutlet weak var closeButton: UIButton!
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        closeButton.setTitle("닫기", for: .normal)
         
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        closeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
     }
 }
