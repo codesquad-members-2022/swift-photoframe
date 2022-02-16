d84632213f65f740dbf803d535e7b83747e5642f
sol <hskim6521@naver.com>
2022-02-16 11:20:20 +0900

    Feat: 모든 ViewController class에 뷰의 라이프사이클을 확인하기 위한 콜백함수 및 코드 추가

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -11,8 +11,25 @@ class YellowViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(#file, #line, #function, #column)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
     @IBAction func nextButtonTouched(_ sender: UIButton) {
         let blueModalVC = self.storyboard?.instantiateViewController(withIdentifier: "blueModal")
         blueModalVC?.modalTransitionStyle = .flipHorizontal
