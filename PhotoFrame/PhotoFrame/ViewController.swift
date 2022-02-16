2ef10a71d1f13cc18b694d66ed14c543d7ac96fa
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-17 02:12:28 +0900

    ViewController 클래스 수정
    - firstLabel, nextButton에 대한 setter 메소드 각각 구현

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -34,18 +34,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
         
-        //라벨에 보여질 텍스트, 속성 설정
+        setFirstLabel()
+        setNextButton()
+        
+    }
+    
+    func setNextButton(){
+        nextButton.center.x = self.view.center.x
+        nextButton.center.y = self.view.center.y*0.5
+        nextButton.setTitle("다음", for: .normal)
+        nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+        nextButton.setTitleColor(.black, for: .normal)
+    }
+    
+    func setFirstLabel(){
         let attributedText = NSMutableAttributedString(string: "Jed의 사진 액자")
         attributedText.addAttributes([.foregroundColor: UIColor.white,
                                       .font: UIFont.systemFont(ofSize: 40)]
                                      , range: NSRange(location: 0, length: 10))
         firstLabel.attributedText = attributedText
         firstLabel.backgroundColor = UIColor.lightGray
-        //가운데 정렬
         firstLabel.center.x = self.view.center.x
         firstLabel.center.y = self.view.center.y
         
-        nextButton.center.x = self.view.center.x
     }
+    
 }
 
