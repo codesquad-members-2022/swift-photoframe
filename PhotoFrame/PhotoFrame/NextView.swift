228dce5a0d787991544c8ffe2eb2978e3bbf330b
Damagucci-Juice <jsr9874@gmail.com>
2022-02-16 14:55:16 +0900

    [Step4] feat : VC에 연결할 class 생성

---
+++ PhotoFrame/PhotoFrame/NextView.swift
@@ -0,0 +1,15 @@
+//
+//  NextView.swift
+//  PhotoFrame
+//
+//  Created by YEONGJIN JANG on 2022/02/16.
+//
+
+import Foundation
+import UIKit
+
+class NextView: UIViewController {
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
