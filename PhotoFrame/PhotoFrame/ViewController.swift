23698a9028ad00bc17ed852b0c4287db68f4e8f0
sanghee <anshee97@naver.com>
2022-02-16 16:51:51 +0900

    Add Third UIButton

--- PhotoFrame/PhotoFrame/ViewController.swift
+++ PhotoFrame/PhotoFrame/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var firstDescription: UILabel!
     
     let firstButton = UIButton(type: UIButton.ButtonType.system) as UIButton
+    let thirdButton = UIButton()
     
-    @objc func buttonAction(_ sender:UIButton!) {
-        print("First Button tapped")
+    @objc func buttonTapped(_ sender: UIButton) {
+        print("\(sender.titleLabel?.text) Button tapped")
+        
+        switch sender {
+        case firstButton:
+            self.firstLabel.text = "첫번째 버튼"
+        default:
+            self.firstLabel.text = "세번째 버튼!"
+        }
     }
     
     
@@ -30,13 +38,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        firstButton.frame = CGRect(x: self.view.center.x, y: 150, width: 150, height: 45)
-        firstButton.backgroundColor = UIColor.systemPink
-        firstButton.setTitle("Tap me!", for: UIControl.State.normal)
-        firstButton.tintColor = .white
-        firstButton.addTarget(self, action: #selector(self.buttonAction), for: .touchUpInside)
-        
-        
+        self.firstButton.frame = CGRect(x: self.view.center.x, y: 150, width: 150, height: 45)
+        self.firstButton.backgroundColor = UIColor.systemPink
+        self.firstButton.setTitle("Tap me!", for: UIControl.State.normal)
+        self.firstButton.tintColor = .white
+        self.firstButton.addTarget(self, action: #selector(self.buttonTapped), for: .touchUpInside)
         
         // subView 추가
         self.view.addSubview(firstButton)
@@ -57,8 +63,6 @@ class ViewController: UIViewController {
         self.firstLabel.backgroundColor = UIColor(red: 250 / 255.0, green: 197 / 255.0, blue: 210 / 255.0, alpha: 1)
         self.firstLabel.backgroundColor = UIColor(cgColor: CGColor(red: 250 / 255.0, green: 197 / 255.0, blue: 210 / 255.0, alpha: 1))
         
-//        self.firstLabel.font = UIFont.systemFont(ofSize: 40)
-//        self.firstLabel.font = UIFont.italicSystemFont(ofSize: 40)
         self.firstLabel.font = UIFont.boldSystemFont(ofSize: 40)
         
         // 세로 위치 중 가운데로 배치
@@ -67,12 +71,26 @@ class ViewController: UIViewController {
         // 가로 위치 중 가운데로 배치
         self.firstLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        
         // firstDescription 속성
         self.firstDescription.textColor = UIColor.systemGray
         self.firstDescription.font = UIFont.systemFont(ofSize: 15)
     
         self.firstDescription.topAnchor.constraint(equalTo: firstLabel.topAnchor, constant: 60).isActive = true
         self.firstDescription.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        
+        // ThirdButton
+        self.view.addSubview(thirdButton)
+        thirdButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        thirdButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        thirdButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
+        thirdButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        thirdButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 150).isActive = true
+        
+        thirdButton.setTitle("세번째 버튼입니당", for: .normal)
+        thirdButton.setTitleColor(.white, for: .normal)
+        thirdButton.backgroundColor = .purple
+        thirdButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
     }
 }
