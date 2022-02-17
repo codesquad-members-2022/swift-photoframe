54d99064e4752f249862192f6451b2834a1781be
BumgeunSong <isp1195@gmail.com>
2022-02-17 18:12:37 +0900

    Restore Gray ViewController

---
+++ PhotoFrame/PhotoFrame/GrayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GrayViewController.swift
+//  PhotoFrame
+//
+//  Created by Bumgeun Song on 2022/02/17.
+//
+
+import UIKit
+
+class GrayViewController: UIViewController {
+    @IBAction func showButtonTouched(_ sender: UIButton) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let yellowViewController = storyBoard.instantiateViewController(withIdentifier: "YellowViewController")
+        show(yellowViewController, sender: self)
+    }
+
+
+    @IBAction func closeButtonTouched(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        print("Gray View did disappear")
+        print(#file, #line, #function, #column)
+    }
+}
