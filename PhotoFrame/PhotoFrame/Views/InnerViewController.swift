215cb1d1f6df51a584201fb2705dc8ee94cac305
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-17 21:53:40 +0900

    [ADD] Two Scenes and connecting segues

---
+++ PhotoFrame/PhotoFrame/Views/InnerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InnerViewController.swift
+//  PhotoFrame
+//
+//  Created by Kai Kim on 2022/02/17.
+//
+
+import UIKit
+
+class InnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ PhotoFrame/PhotoFrame/Views/SecondInnerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondInnerViewController.swift
+//  PhotoFrame
+//
+//  Created by Kai Kim on 2022/02/17.
+//
+
+import UIKit
+
+class SecondInnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+
+}
