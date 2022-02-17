973883b74d06a43bae45d02a3cc2b19206e80d1b
sanghee <anshee97@naver.com>
2022-02-18 04:53:00 +0900

    Add MintViewController

---
+++ PhotoFrame/PhotoFrame/MintViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MintViewController.swift
+//  PhotoFrame
+//
+//  Created by Selina on 2022/02/18.
+//
+
+import UIKit
+
+class MintViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
