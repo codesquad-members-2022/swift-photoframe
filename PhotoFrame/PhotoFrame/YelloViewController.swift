87cbbe00a467e445cb5e1d85f59ca38ddeaec4f6
Damagucci-Juice <jsr9874@gmail.com>
2022-02-17 14:28:04 +0900

    [Step5] Add : YellowViewController.swift

---
+++ PhotoFrame/PhotoFrame/YelloViewController.swift
@@ -0,0 +1,15 @@
+//
+//  YelloViewController.swift
+//  PhotoFrame
+//
+//  Created by YEONGJIN JANG on 2022/02/17.
+//
+
+import UIKit
+
+class YelloViewController: UIViewController {
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
