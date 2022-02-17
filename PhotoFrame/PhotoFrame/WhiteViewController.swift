9cc6d022f3b8d602cb869b3c926a6dac09b8111d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-18 02:27:30 +0900

    버튼 누를 때마다 랜덤으로 이미지 나오도록 액션 설정
    - 버튼 누를때마다 1~22 사이의 랜덤 숫자 도출해서 이에 해당하는 이미지파일 가져옴
    - UIImage를 가져와서 imageView.image에 할당

--- PhotoFrame/PhotoFrame/WhiteViewController.swift
+++ PhotoFrame/PhotoFrame/WhiteViewController.swift
@@ -7,7 +7,7 @@ class WhiteViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     
     @IBAction func nextButtonTouched(_ sender: UIButton) {
-        
+        imageView.image = getRandomImage(number: Int.random(in: 1...22))
     }
     
     override func viewDidLoad() {
@@ -29,6 +29,7 @@ class WhiteViewController: UIViewController {
         imageView.center.x = self.view.center.x
         imageView.center.y = self.view.center.y*0.7
         imageView.backgroundColor = .lightGray
+        imageView.contentMode = .scaleToFill
     }
     
     func setNextButton(){
@@ -36,5 +37,10 @@ class WhiteViewController: UIViewController {
         nextButton.center.y = self.view.center.y*1.3
         nextButton.setTitle("다음", for: .normal)
         nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+    }
+    
+    func getRandomImage(number: Int)-> UIImage?{
+        let fileName: String = number<10 ? "0\(number)" : String(number)
+        return UIImage(named: "Demo Images/\(fileName).jpg")
     }
 }
