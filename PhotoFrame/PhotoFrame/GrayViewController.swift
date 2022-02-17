b3136c83928216bb1e3b222d2b7964d3682502f0
Damagucci-Juice <jsr9874@gmail.com>
2022-02-17 14:30:20 +0900

    [Step5] Add : GrayViewContorller.swift and close Button

---
+++ PhotoFrame/PhotoFrame/GrayViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GrayViewController.swift
+//  PhotoFrame
+//
+//  Created by YEONGJIN JANG on 2022/02/17.
+//
+
+import UIKit
+
+class GrayViewController: UIViewController {
+    
+    @IBAction func closeButtonTouced(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
