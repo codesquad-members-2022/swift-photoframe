c91534e0fe7c175cd841c4a563a7e6057f3d146a
P_SeoB <akdlsjtm21@naver.com>
2022-02-17 21:28:08 +0900

    [Delete] TabBarController

---
+++ PhotoFrame/PhotoFrame/NavigationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewController.swift
+//  PhotoFrame
+//
+//  Created by 박진섭 on 2022/02/17.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
