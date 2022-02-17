5e78c46c48d4262af657b495763f08f1da99cde6
kimdongjune <jun36817@naver.com>
2022-02-17 19:29:03 +0900

    rename viewcontrollers

--- PhotoFrame/PhotoFrame/Scene1ViewController.swift
+++ PhotoFrame/PhotoFrame/Scene1ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Scene1ViewController: UIViewController {
     
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet weak var firstDescription: UILabel!
