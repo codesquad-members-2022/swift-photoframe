2dd3aef6de724a4871c495eac05b67086e2d5000
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-17 21:53:40 +0900

    [ADD] Two Scenes and connecting segues

---
+++ PhotoFrame/PhotoFrame/Views/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Kai Kim on 2022/02/15.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ PhotoFrame/PhotoFrame/Views/myTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  myTabBarViewController.swift
+//  PhotoFrame
+//
+//  Created by Kai Kim on 2022/02/15.
+//
+
+import UIKit
+
+class myTabBarViewController: UITabBarController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#file, #line, #function, #column)
+        
+    }
+   
+}
