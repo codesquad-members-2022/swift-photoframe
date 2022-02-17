f2950c09bf7f906665b55060d357d103136aee25
kimdongjune <jun36817@naver.com>
2022-02-17 16:32:29 +0900

    NavigationViewController의 자식ViewController들 확인하는 코드를 작성하였습니다.

--- PhotoFrame/PhotoFrame/GreenViewController.swift
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -16,10 +16,21 @@ class GreenViewController: UIViewController{
         super.viewDidLoad()
         self.closeButtonText(button: closeButton)
         self.backGround(color: .green)
+        self.printNavigationChild()
     }
     
     @IBAction func closeButtonTapped(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func printNavigationChild(){
+        guard let vcArr = self.navigationController?.viewControllers else{
+            return
+        }
+        print("NavigationViewController의 ViewController는 \(vcArr.count)개 입니다.")
+        for vc in vcArr{
+            print(vc)
+        }
     }
 }
 
