387bf65d011636787db54e3ba878211757427023
shingha <shingha1124@gmail.com>
2022-02-17 10:12:42 +0900

    [Refactor] 추가학습 네이게이션 연결, 기본 틀 구성

--- PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/StudyTableViewController.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/StudyTableViewController.swift
@@ -9,7 +9,12 @@ import Foundation
 import UIKit
 
 class StudyTableViewController: UITableViewController {
-        
+    let studyData: [StudyModel] = [
+        StudyModel("버튼에 액션을 여러개 추가할 수 있을까?", identifier: "StudyViewController1"),
+        StudyModel("버튼에 액션을 여러개 추가할 수 있을까?1", identifier: "StudyViewController1"),
+        StudyModel("버튼에 액션을 여러개 추가할 수 있을까?2", identifier: "StudyViewController1")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,16 +24,25 @@ class StudyTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return studyData.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "StudyTableViewCell", for: indexPath) as? StudyTableViewCell else { return UITableViewCell() }
         
-        cell.setData(indexPath.row)
+        cell.delegate = self
+        cell.setData(studyData[indexPath.row])
         return cell
     }
-    @IBAction func onCellTapped(_ sender: StudyTableViewCell) {
-        print("afdasfadsf")
+}
+
+extension StudyTableViewController: StudyTableViewCellEvent {
+    func onCellTapped(data: StudyModel?) {
+        guard let data = data,
+        let viewController = self.storyboard?.instantiateViewController(withIdentifier: data.identifier) else {
+            return
+        }
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 }
+
