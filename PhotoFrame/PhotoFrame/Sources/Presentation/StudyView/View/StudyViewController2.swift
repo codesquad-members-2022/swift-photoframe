ab8459fe8e9fa7a05e671fc1c275b7c7bf752981
shingha <shingha1124@gmail.com>
2022-02-17 10:38:25 +0900

    [Refactor] 사용하지 않는 코드 제거 및 이전에 만들었던 테스트 코드 복구

--- PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/View/StudyViewController2.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/View/StudyViewController2.swift
@@ -8,10 +8,30 @@
 import Foundation
 import UIKit
 
+class CustomButton: UIButton {
+    var testValue: String = ""
+}
+
 class StudyViewController2: UIViewController {
+    
+    @IBOutlet weak var testButton1: CustomButton!
+    @IBOutlet weak var testButton2: CustomButton!
+    @IBOutlet weak var testButton3: CustomButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        testButton1.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
+        testButton1.testValue = "testButton1"
+        testButton2.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
+        testButton2.testValue = "testButton2"
+        testButton3.addTarget(self, action: #selector(onTestButtonTapped(_:)), for: .touchUpInside)
+        testButton3.testValue = "testButton3"
+    }
+    
+    @objc
+    func onTestButtonTapped(_ sender: CustomButton) {
+        print("onTestButtonTapped - \(sender.testValue)")
     }
 }
 
