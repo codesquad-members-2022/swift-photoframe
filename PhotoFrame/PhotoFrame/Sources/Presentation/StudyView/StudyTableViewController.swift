aec3a82d0fe6db818d21475adb6604cf3ebc9dff
shingha <shingha1124@gmail.com>
2022-02-17 16:34:10 +0900

    [Refactor] 기존 제작한 FirstView도 추가학습 네비게이션컨트롤러로 이동

--- PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/StudyTableViewController.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/StudyTableViewController.swift
@@ -9,7 +9,8 @@ import Foundation
 import UIKit
 
 class StudyTableViewController: UITableViewController {
-    let studyData: [StudyModel] = [
+    let studyDatas: [StudyModel] = [
+        StudyModel("네비게이션 컨트롤러 테스트", identifier: "FirstViewController"),
         StudyModel("버튼에 액션을 여러개 추가할 수 있을까?", identifier: "StudyViewController1"),
         StudyModel("여러 버튼을 동시에 하나의 액션에 연결할 수 있을까?", identifier: "StudyViewController2"),
         StudyModel("화면 전환이 이루어지는 사이에 뷰컨트롤러 라이프사이클이 어떻게 변화하는지 학습한다.", identifier: "StudyViewController3")
@@ -24,14 +25,14 @@ class StudyTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return studyData.count
+        return studyDatas.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "StudyTableViewCell", for: indexPath) as? StudyTableViewCell else { return UITableViewCell() }
         
         cell.delegate = self
-        cell.setData(studyData[indexPath.row])
+        cell.setData(studyDatas[indexPath.row])
         return cell
     }
 }
