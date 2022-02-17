155f38e292bad67454bce3608dd8311105be70cf
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-18 00:50:32 +0900

    WhiteViewController 생성 및 디자인
    - 탭바의 Item2 Scene에 생성한 WhiteViewController 연결
    - 해당 View 디자인

---
+++ PhotoFrame/PhotoFrame/WhiteViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class WhiteViewController: UIViewController {
+
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setAlbumLabel()
+        setImageView()
+        setNextButton()
+    }
+    
+    func setAlbumLabel(){
+        albumLabel.center.x = self.view.center.x
+        albumLabel.center.y = self.view.center.y*1.15
+        albumLabel.text = "Photo Album"
+        albumLabel.textAlignment = .center
+        albumLabel.font = UIFont.systemFont(ofSize: 40)
+    }
+    
+    func setImageView(){
+        imageView.center.x = self.view.center.x
+        imageView.center.y = self.view.center.y*0.7
+        imageView.backgroundColor = .lightGray
+    }
+    
+    func setNextButton(){
+        nextButton.center.x = self.view.center.x
+        nextButton.center.y = self.view.center.y*1.3
+        nextButton.setTitle("다음", for: .normal)
+        nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+    }
+}
