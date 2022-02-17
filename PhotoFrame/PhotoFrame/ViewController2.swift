67dc96f4651a4f6095525b6435feee82ef30d74f
Damagucci-Juice <jsr9874@gmail.com>
2022-02-18 01:23:01 +0900

    [Step7] Feat : when click 다음 button, show random image

--- PhotoFrame/PhotoFrame/ViewController2.swift
+++ PhotoFrame/PhotoFrame/ViewController2.swift
@@ -9,16 +9,19 @@ import Foundation
 import UIKit
 
 class ViewController2: UIViewController {
+    @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var photoLabel: UILabel!
     @IBAction func nextButtonTouched(_ sender: Any) {
         let random_number: Int = Int.random(in: 1...22)
-        self.photoImageView.image = UIImage(named: "\(random_number).jpg")
+        if random_number < 10 {
+            self.photoImageView.image = UIImage(named: "0\(random_number).jpg")
+        } else {
+            self.photoImageView.image = UIImage(named: "\(random_number).jpg")
+        }
     }
     @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(#file, #line, #function, #column)
-        print("Hello World cont2")
+        self.nextButtonTouched(self.nextButton)
     }
 }
