f614ac7eb4c4cf43a5fa50eb5168ce083331c376
Jihee hwang <hwangjihee94@gmail.com>
2022-02-17 17:46:05 +0900

    Complete : Step5
    Add close button and learn about ViewController

--- Photo Frame/Photo Frame/ViewController.swift
+++ Photo Frame/Photo Frame/ViewController.swift
@@ -28,14 +28,5 @@ class ViewController: UIViewController {
         self.photoLabel.font = UIFont(name: "Futura Bold", size: 45)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
