3343352fa0b4a2d82857ad9b268490dbc469d20f
BumgeunSong <isp1195@gmail.com>
2022-02-17 12:39:46 +0900

    Delete unnecessary comments & function

--- PhotoFrame/PhotoFrame/GrayViewController.swift
+++ PhotoFrame/PhotoFrame/GrayViewController.swift
@@ -14,15 +14,12 @@ class GrayViewController: UIViewController {
         show(yellowViewController, sender: self)
     }
     
-    
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,16 +41,4 @@ class GrayViewController: UIViewController {
         print("Gray View did disappear")
         print(#file, #line, #function, #column)
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
