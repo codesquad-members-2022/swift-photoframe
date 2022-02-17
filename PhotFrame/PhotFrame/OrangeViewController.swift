ef8ca0530727347c4ba283593b8d305069da53cb
LeeJoobang <hii5074@gmail.com>
2022-02-17 17:56:54 +0900

    클래스 내 콜백함수 추가  - 클래스(3종) :  ViewController, RedViewController, OrangeViewController  - 콜백함수(4종) : viewWillAppear(), viewDidAppear(), viewWillDisappear(), viewDidDisappear()

--- PhotFrame/PhotFrame/OrangeViewController.swift
+++ PhotFrame/PhotFrame/OrangeViewController.swift
@@ -17,4 +17,28 @@ class OrangeViewController: UIViewController {
         super.viewDidLoad()
         print(#file, #line, #function, #column)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 나타날 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 나타났음")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 사라졌음")
+    }
 }
