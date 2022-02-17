1c5a530cd2d103f42e2b9ffffc396061616c01c0
sanghee <anshee97@naver.com>
2022-02-18 04:50:53 +0900

    Add close button and connect IBAction Method

--- PhotoFrame/PhotoFrame/PinkViewController.swift
+++ PhotoFrame/PhotoFrame/PinkViewController.swift
@@ -2,28 +2,20 @@
 //  PinkViewController.swift
 //  PhotoFrame
 //
-//  Created by 안상희 on 2022/02/18.
+//  Created by Selina on 2022/02/18.
 //
 
 import UIKit
 
 class PinkViewController: UIViewController {
 
+    @IBAction func closeButtonTouched(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
