3283151cd0fce7663a1b4bd4c635b5fef51ac3c7
LeeJoobang <hii5074@gmail.com>
2022-02-17 16:22:51 +0900

    함수(4종) 추가  1) viewWillAppear()  2) viewDidAppear()  3) viewWillDisappear()  4) viewDidDisappear()

--- PhotFrame/PhotFrame/ViewController.swift
+++ PhotFrame/PhotFrame/ViewController.swift
@@ -16,8 +16,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.firstLabel.text = "joobang의 사진액자"
         print(#file, #line, #function, #column)
+        print("뷰 로드됨")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 나타날 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 나타났음")
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+        print("뷰 사라졌음")
+    }
+    
     @IBAction func nextButtonTouched(_ sender: Any) {
         self.firstLabel.textColor = UIColor.blue
         self.firstLabel.backgroundColor = UIColor.yellow
