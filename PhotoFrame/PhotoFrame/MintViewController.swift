ef957dd6570c77c071a750e2b4514208488117d4
sanghee <anshee97@naver.com>
2022-02-18 12:14:37 +0900

    Add close button and button action

--- PhotoFrame/PhotoFrame/MintViewController.swift
+++ PhotoFrame/PhotoFrame/MintViewController.swift
@@ -9,8 +9,36 @@ import UIKit
 
 class MintViewController: UIViewController {
 
+    let closeButton = UIButton(type: UIButton.ButtonType.roundedRect) as UIButton
+    
+    @objc func closeButtonTouched(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+//        self.view.addSubview(closeButton)
+//        // 이렇게 하는 방법은 addSubview의 위치가 상관없고, translatesAutoresizingMaskIntoConstraints는 true여야한다. false이면 위치가 지정이 안돼. 그냥
+//        self.closeButton.frame = CGRect(x: 20, y: 20, width: 100, height: 45)
+//        self.closeButton.backgroundColor = .white
+//        self.closeButton.setTitle("닫기", for: .normal)
+//        self.closeButton.tintColor = .black
+//        self.closeButton.addTarget(self, action: #selector(self.closeButtonTouched(_:)), for: .touchUpInside)
+        
+        
+        // constraint를 주는 방법은 subView가 반드시 제일 먼저 와야하고, translatesAUtoresizingMaskIntoConstraints는 false여야한다. true이면 안된다.
+        self.view.addSubview(closeButton)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        closeButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 150).isActive = true
+        closeButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        closeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        closeButton.backgroundColor = .white
+        closeButton.setTitle("닫기", for: .normal)
+        closeButton.tintColor = .black
+        closeButton.addTarget(self, action: #selector(self.closeButtonTouched(_:)), for: .touchUpInside)
     }
 }
