a15232bfe775d8e411e8131dbb73b6bd245fb9cd
shingha <shingha1124@gmail.com>
2022-02-18 10:41:14 +0900

    [Review] UIAction을 이용한 버튼 이벤트 연결

--- PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/View/StudyViewController1.swift
+++ PhotoFrame/PhotoFrame/Sources/Presentation/StudyView/View/StudyViewController1.swift
@@ -15,11 +15,21 @@ class StudyViewController1: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //addTarget으로 버튼 이벤트 연결하기
         testButton.addTarget(self, action: #selector(onTestButtonTapped_1), for: .touchUpInside)
         testButton.addTarget(self, action: #selector(onTestButtonTapped_2), for: .touchUpInside)
         testButton.addTarget(self, action: #selector(onTestButtonTapped_3), for: .touchUpInside)
-        testButton.addTarget(self, action: #selector(onTestButtonTapped_4), for: .touchUpInside)
-        testButton.addTarget(self, action: #selector(onTestButtonTapped_5), for: .touchUpInside)
+        
+        //addAction으로 버튼 이벤트 연결하기 1
+        testButton.addAction(UIAction(handler: { _ in
+            self.onTestButtonTapped_9()
+        }), for: .touchUpInside)
+
+        //addAction으로 버튼 이벤트 연결하기 2
+        let action = UIAction(title: "testEvent") { _ in
+            self.onTestButtonTapped_10()
+        }
+        testButton.addAction(action, for: .touchUpInside)
     }
     
     @objc
@@ -37,13 +47,23 @@ class StudyViewController1: UIViewController {
         print("onTestButtonTapped_3")
     }
     
-    @objc
-    func onTestButtonTapped_4() {
-        print("onTestButtonTapped_4")
+    @IBAction func onTestButtonTapped_6(_ sender: Any) {
+        print("@IBAction onTestButtonTapped_6")
     }
     
-    @objc
-    func onTestButtonTapped_5() {
-        print("onTestButtonTapped_5")
+    @IBAction func onTestButtonTapped_7(_ sender: Any) {
+        print("@IBAction onTestButtonTapped_7")
+    }
+    
+    @IBAction func onTestButtonTapped_8(_ sender: Any) {
+        print("@IBAction onTestButtonTapped_8")
+    }
+    
+    func onTestButtonTapped_9() {
+        print("UIAction onTestButtonTapped_9")
+    }
+    
+    func onTestButtonTapped_10() {
+        print("UIAction onTestButtonTapped_10")
     }
 }
