c11688d549beb5bbba2ef05d40b634cd3096c791
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-18 16:56:54 +0900

    [Add] SecondViewController 추가, UIimageView 추가

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by 김상혁 on 2022/02/18.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
