98abcd06745add110f4a21cd62d779983754fb4d
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-02-18 14:10:24 +0900

    Practice connecting ViewController
    뷰 컨트롤러를 연결하는 과정을 수행.
    추가로, 프로젝트 폴더 안에 프로젝트 폴더 안에 프로젝트가 들어있던 것을 뒤늦게 발견.
    모든 프로젝트 파일들을 정리하고 새로 커밋과 병합을 진행.
    이후 추가 Conflict 발생 예정이라 기존 파일들은 원격 저장소의 기준대로 진행될 예정.

---
+++ PhotoFrame/PhotoFrame/BrownViewController.swift
@@ -0,0 +1,39 @@
+//
+//  BrownViewController.swift
+//  PhotoFrame
+//
+//  Created by 백상휘 on 2022/02/18.
+//
+
+import UIKit
+
+class BrownViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
