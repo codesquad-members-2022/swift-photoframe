2c6b1702513b95c2a2501309f6a2953098699a72
Song TaeHwan <axoghks@gmail.com>
2022-02-18 11:29:46 +0900

    feat: Remove undesired button targets from FirstViewController

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -17,13 +17,30 @@ class FirstViewController: UIViewController {
         self.photoLabel.text = "Welcome!"
         self.photoLabel.font = UIFont(name: "Chalkduster", size: 42)
         self.photoLabel.textColor = self.photoLabel.textColor.withAlphaComponent(0.8)
+        print("Root View", #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Root View", #function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Root View", #function)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Root View", #function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Root View", #function)
     }
 
     // MARK: - Handlers
-    @IBAction func didEndOnExit(_ sender: Any) {
-        print("Did End On Exit")
-    }
-    
     @IBAction func editingChanged(_ sender: Any) {
         print("Editing Change - 키보드 입력 시 트리거")
     }
@@ -35,49 +52,4 @@ class FirstViewController: UIViewController {
     @IBAction func editingDidEnd(_ sender: Any) {
         print("Editing Did End - Text Field 가 blur 될 때 트리커")
     }
-
-    @IBAction func primaryActionTriggered(_ sender: Any) {
-        print("Primary Action Triggered")
-    }
-    
-    @IBAction func touchCanceled(_ sender: Any) {
-        print("Touch Cancel")
-    }
-    
-    @IBAction func touchedDownRepeat(_ sender: Any) {
-        print("Touch Down Repeat")
-    }
-    
-    @IBAction func TouchedDown(_ sender: Any) {
-        print("Touch Down - 탭을 누를 때 트리거")
-    }
-    
-    @IBAction func dragExited(_ sender: Any) {
-        print("Drag Exit")
-    }
-    
-    @IBAction func dragEntered(_ sender: Any) {
-        print("Drag Enter")
-    }
-    
-    @IBAction func valueChanged(_ sender: Any) {
-        print("Value Changed")
-    }
-    
-    @IBAction func draggedInside(_ sender: Any) {
-        print("Touch Drag Inside - 탭을 누른 상태에서 드래그할 때 트리거")
-    }
-    
-    @IBAction func draggedOutside(_ sender: Any) {
-        print("Touch Drag Outside - 드래그 중에 UI 요소 영역밖으로 나갈 때 트리거")
-    }
-    
-    @IBAction func touchedUpInside(_ sender: Any) {
-        print("Touch Up Inside - UI 요소 영역 안에서 탭을 땔 때 트리거")
-    }
-    
-    @IBAction func touchedUpOutside(_ sender: Any) {
-        print("Touch Up Outside - UI 요소 영역 밖에서 탭을 땔 때 트리거")
-    }
-    
 }
