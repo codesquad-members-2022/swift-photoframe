e3222bf396c8de08f12e98f82c9200710c168998
bibi <bibi6666667@kakao.com>
2022-02-18 15:38:00 +0900

    step5 ) LightGrayViewController, DarkGrayViewController에 화면 관련 콜백 함수 추가

--- PhotoFrame/PhotoFrame/LightGrayViewController.swift
+++ PhotoFrame/PhotoFrame/LightGrayViewController.swift
@@ -15,6 +15,22 @@ class LightGrayViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+        
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
