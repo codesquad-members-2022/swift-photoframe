0845f31055d57f798882ac1aebf452fefd585722
Jason <ggj00125@gmail.com>
2022-02-18 15:29:02 +0900

    Fix: Step2 Label 추가 및 텍스트 출력 확인

--- PhotoFrame/PhotoFrame/FirstViewController.swift
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var firstLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.firstLabel.text = "Jason의 사진액자"
+        //글자색, 배경색, 투명도, 글자 크기
+        self.firstLabel.tex
     }
 }
 
