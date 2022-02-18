93ff769ea89ccd62ded1dd15b800892a10b67a9e
Song TaeHwan <axoghks@gmail.com>
2022-02-18 11:30:31 +0900

    feat: Add viwe life cycle methods onto all of view controller

--- PhotoFrame/PhotoFrame/GreenViewController.swift
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -1,40 +1,36 @@
 //
-//  PurpleViewController.swift
+//  GreenViewController.swift
 //  PhotoFrame
 //
-//  Created by 송태환 on 2022/02/14.
+//  Created by 송태환 on 2022/02/18.
 //
 
 import UIKit
 
-class PurpleViewController: UIViewController {
+class GreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Third View", #function)
+        print(#fileID, #function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("Third View", #function)
+        print(#fileID, #function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("Third View", #function)
+        print(#fileID, #function)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("Third View", #function)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("Third View", #function)
+        print(#fileID, #function)
     }
 
-    @IBAction func closeButtonTouched(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#fileID, #function)
     }
 }
