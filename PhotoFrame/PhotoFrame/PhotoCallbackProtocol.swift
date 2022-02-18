42354b844a855241d9042b29dc79a8a817fea7c6
kimdongjune <jun36817@naver.com>
2022-02-18 15:50:44 +0900

    기기의 이미지 가져오는 기능을 구현하였습니다.

---
+++ PhotoFrame/PhotoFrame/PhotoCallbackProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCallbackProtocol.swift
+//  PhotoFrame
+//
+//  Created by 김동준 on 2022/02/18.
+//
+
+import Foundation
+import UIKit
+
+protocol PhotoCallbackProtocol{
+    func photoCallback(_ image: UIImage)
+}
