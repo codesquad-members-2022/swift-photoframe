c9d4b35ddec7024ec800a023bfafc68b2f578391
Jason <ggj00125@gmail.com>
2022-02-18 15:15:31 +0900

    Fix: 앞의 PR문제로 인해 Step2과정이 없어져 시작
    viewController이 custom class에 제대로 연결되지않아 FirstViewController.swift 파일 추가

---
+++ PhotoFrame/PhotoFrame/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  PhotoFrame
+//
+//  Created by 이건행 on 2022/02/18.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
