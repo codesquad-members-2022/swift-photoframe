af562e8c9ea067e508ea7ac6d8ed68d342d75ca2
kimdongjune <jun36817@naver.com>
2022-02-18 11:26:55 +0900

    버튼 터치 시 이미지가 변경되는 코드 작성완료 및 이미지 추가

--- PhotoFrame/PhotoFrame/Scene2ViewController.swift
+++ PhotoFrame/PhotoFrame/Scene2ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class Scene2ViewController: UIViewController {
 
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var setImageButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAlbumLabelUI()
+        setImageButtonUI()
     }
-
+    private func setAlbumLabelUI(){
+        albumLabel.text = "Ebony의 사진앨범!"
+        albumLabel.font = UIFont.systemFont(ofSize: 24)
+        albumLabel.textAlignment = .center
+    }
+    
+    private func setImageButtonUI(){
+        setImageButton.setTitle("이미지 변경", for: .normal)
+        setImageButton.contentHorizontalAlignment = .center
+    }
+    
+    @IBAction func setImageButtonTapped(_ sender: UIButton) {
+        sender.setTitle("다른 이미지", for: .normal)
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: randomImageName())
+    }
+    
+    private func randomImageName() -> String{
+        let randomNum = Int.random(in: 1 ... 22)
+        return randomNum < 10 ?  "0\(randomNum).jpg" : "\(randomNum).jpg"
+    }
+    
 }
 
