9f57f9b2d7ff9db06c44df765e2228876f7e18f5
bibi <bibi6666667@kakao.com>
2022-02-18 15:23:35 +0900

    step5 ) 닫기 버튼 추가 및 DarkGrayViewController에 IBAction 연결

--- PhotoFrame/PhotoFrame/DarkGrayViewController.swift
+++ PhotoFrame/PhotoFrame/DarkGrayViewController.swift
@@ -15,7 +15,10 @@ class DarkGrayViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
