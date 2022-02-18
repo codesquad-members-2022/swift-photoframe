1ff609bfac1e042af9d2781f32868ef650caeb5c
wnsxor1993 <wnsxor1993@naver.com>
2022-02-18 14:58:11 +0900

    Feat: 앨범 선택 기능 구현 - UIImagePickerController / PHPickerViewController 각각 활용하여 선택 기능 구현

--- PhotoFrame/PhotoFrame/SecondSceneViewController.swift
+++ PhotoFrame/PhotoFrame/SecondSceneViewController.swift
@@ -6,13 +6,40 @@
 //
 
 import UIKit
+import PhotosUI
 
 class SecondSceneViewController: UIViewController {
     private var imageNum: String = "01.jpg"
     private var uiImage: UIImage?
+    private var imagePicker = PHPickerConfiguration()
     
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var shadowView: UIView!
+    
+    func changeImageNumber(number: Int) -> String{
+        var imageNum: String{
+            if number < 10{
+                let index: String = "0\(number).jpg"
+                return index
+            } else{
+                let index: String = "\(number).jpg"
+                return index
+            }
+        }
+        return imageNum
+    }
+    
+    func makeImageNumber(){
+        var number = Int.random(in: 1...22)
+        var randomImageNum: String = changeImageNumber(number: number)
+        
+        while imageNum == randomImageNum{
+            number = Int.random(in: 1...22)
+            randomImageNum = changeImageNumber(number: number)
+        }
+        
+        imageNum = randomImageNum
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,7 +80,6 @@ class SecondSceneViewController: UIViewController {
         }
     }
     
-    
     @IBAction func nextImageButtonNotRandom(_ sender: UIButton) {
         let presentNum = imageNum.components(separatedBy: ".")[0]
         
@@ -69,28 +95,35 @@ class SecondSceneViewController: UIViewController {
         }
     }
     
-    func changeImageNumber(number: Int) -> String{
-        var imageNum: String{
-            if number < 10{
-                let index: String = "0\(number).jpg"
-                return index
-            } else{
-                let index: String = "\(number).jpg"
-                return index
+    @IBAction func selectButtonTouched(_ sender: UIButton) {
+        imagePicker.selectionLimit = 1
+        imagePicker.filter = .images
+        
+        let picker = PHPickerViewController(configuration: imagePicker)
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+}
+
+extension SecondSceneViewController: PHPickerViewControllerDelegate{
+    // 함수 내부는 선택된 하나의 이미지를 imageView에 출력시키는 로직 구현
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        // 선택 완료 or cancel 시, view dismiss
+        picker.dismiss(animated: true)
+        
+        // 선택된 결과값들 중 첫번째 결과값의 itemProvider를 할당
+        let itemProvider = results.first?.itemProvider
+        
+        // 가져온 itemProvider 값이 nil이 아닌지, 이를 통해서 UIImage로 지정한 타입으로 로드 가능한 지 확인
+        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self){
+            // 지정한 타입으로 로드. completionHandler로 NSItemProviderReading과 error 전달
+            itemProvider.loadObject(ofClass: UIImage.self){ (image, error) in
+                // itemProvider는 background async 작업. UI 관련 업데이트는 main 스레드.
+                DispatchQueue.main.async {
+                    self.photoImageView.image = image as? UIImage
+                }
             }
-        }
-        return imageNum
+        } else{}
     }
-    
-    func makeImageNumber(){
-        var number = Int.random(in: 1...22)
-        var randomImageNum: String = changeImageNumber(number: number)
-        
-        while imageNum == randomImageNum{
-            number = Int.random(in: 1...22)
-            randomImageNum = changeImageNumber(number: number)
-        }
-        
-        imageNum = randomImageNum
-    }
+
 }
