bfa0ef69d4ae5cd31dba9a5e2890b15bd8a33526
Lee SeEun <sally4405@naver.com>
2022-02-18 17:43:00 +0900

    [step7] feat: tab bar의 두번째 scene인 yellow view에 뷰컨트롤러를 추가하고 image view와 아울렛을 추가

---
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
