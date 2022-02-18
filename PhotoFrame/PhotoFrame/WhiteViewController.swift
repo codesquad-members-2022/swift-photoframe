0a232df893ac2d689830ba3c1db5120485d1a68c
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-18 11:35:45 +0900

    PhotoFrame ImageView 추가

--- PhotoFrame/PhotoFrame/WhiteViewController.swift
+++ PhotoFrame/PhotoFrame/WhiteViewController.swift
@@ -5,6 +5,7 @@ class WhiteViewController: UIViewController {
     @IBOutlet weak var albumLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var frameView: UIImageView!
     
     @IBAction func nextButtonTouched(_ sender: UIButton) {
         imageView.image = getRandomImage(number: Int.random(in: 1...22))
@@ -15,6 +16,13 @@ class WhiteViewController: UIViewController {
         setAlbumLabel()
         setImageView()
         setNextButton()
+        setFrameView()
+    }
+    
+    func setFrameView(){
+        frameView.image = UIImage(named: "Demo Images/photoframe-border.png")
+        frameView.center.x = self.view.center.x
+        frameView.center.y = self.view.center.y*0.7
     }
     
     func setAlbumLabel(){
