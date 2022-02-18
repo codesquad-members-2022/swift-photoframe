4cccc1b3a3a0af01567d5d40e8e2b2c9f1ab1576
BumgeunSong <isp1195@gmail.com>
2022-02-18 11:36:49 +0900

    Add Image View in second scene & connect it as IBOutlet

---
+++ PhotoFrame/PhotoFrame/PhotoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoViewController.swift
+//  PhotoFrame
+//
+//  Created by Bumgeun Song on 2022/02/18.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
