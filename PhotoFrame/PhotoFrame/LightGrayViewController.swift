f1e5d3b1577e97f70a7370e8a4967e37a6100516
bibi <bibi6666667@kakao.com>
2022-02-18 15:16:48 +0900

    step5 ) LightGrayViewController에 닫기 버튼의 IBAction 추가

--- PhotoFrame/PhotoFrame/LightGrayViewController.swift
+++ PhotoFrame/PhotoFrame/LightGrayViewController.swift
@@ -15,7 +15,10 @@ class LightGrayViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
