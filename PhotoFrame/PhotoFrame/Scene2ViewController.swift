1d00a971e05ab89254d83a27773256a998fa89be
kimdongjune <jun36817@naver.com>
2022-02-18 15:50:44 +0900

    기기의 이미지 가져오는 기능을 구현하였습니다.

--- PhotoFrame/PhotoFrame/Scene2ViewController.swift
+++ PhotoFrame/PhotoFrame/Scene2ViewController.swift
@@ -14,21 +14,39 @@ class Scene2ViewController: UIViewController {
     @IBOutlet weak var albumLabel: UILabel!
     @IBOutlet weak var setImageButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoFrameImageView: UIImageView!
+    @IBOutlet weak var selectPhotoButton: UIButton!
+    private lazy var photoPickerController = UIImagePickerController()
+    private var photoDelegate: PhotoPickerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setAlbumLabelUI()
-        setImageButtonUI()
+        setButtonsUI()
+        setFrameImageViewUI()
+        setImageFrameZPosition()
     }
+    
     private func setAlbumLabelUI(){
         albumLabel.text = "Ebony의 사진앨범!"
         albumLabel.font = UIFont.systemFont(ofSize: 24)
         albumLabel.textAlignment = .center
     }
     
-    private func setImageButtonUI(){
+    private func setButtonsUI(){
         setImageButton.setTitle("이미지 변경", for: .normal)
         setImageButton.contentHorizontalAlignment = .center
+        selectPhotoButton.setTitle("사진 선택", for: .normal)
+        selectPhotoButton.contentHorizontalAlignment = .center
+    }
+    
+    private func setFrameImageViewUI(){
+        photoFrameImageView.image = UIImage(named: "photoframe-border.jpg")
+    }
+    
+    private func setImageFrameZPosition(){
+        photoFrameImageView.layer.zPosition = 1
+        imageView.layer.zPosition = 10
     }
     
     @IBAction func setImageButtonTapped(_ sender: UIButton) {
@@ -37,10 +55,32 @@ class Scene2ViewController: UIViewController {
         imageView.image = UIImage(named: randomImageName())
     }
     
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        print("hello")
+        if photoPickerController.delegate == nil{
+            setPhotoPickerState()
+            present(photoPickerController, animated: true, completion: nil)
+        }
+        present(photoPickerController, animated: true, completion: nil)
+    }
+    
     private func randomImageName() -> String{
         let randomNum = Int.random(in: 1 ... 22)
         return randomNum < 10 ?  String.init(format: "0%d.jpg", randomNum) : String.init(format: "%d.jpg", randomNum)
     }
     
+    private func setPhotoPickerState(){
+        photoPickerController.sourceType = .photoLibrary
+        photoPickerController.allowsEditing = false
+        photoDelegate = PhotoPickerDelegate(self)
+        photoPickerController.delegate = photoDelegate
+    }
+    
 }
 
+extension Scene2ViewController: PhotoCallbackProtocol{
+    func photoCallback(_ image: UIImage) {
+        imageView.image = image
+    }
+    
+}
