4115bcb038720a0bcc4e320b8f757a8933165264
Jinsujin <sylpid003@gmail.com>
2022-02-20 21:13:20 +0900

    feat: BlueViewController- 뷰 컨트롤러 라이프 사이클 함수 print

--- PhotoFrame/PhotoFrame/BlueViewController.swift
+++ PhotoFrame/PhotoFrame/BlueViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class BlueViewController: UIViewController {
 
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func closeButtonTouched(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#file, #line, #function, #column)
     }
 }
