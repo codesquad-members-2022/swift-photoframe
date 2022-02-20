64d95fc5d087a12f67406a6c8cffbe20360d7b9a
Damagucci-Juice <jsr9874@gmail.com>
2022-02-20 19:46:14 +0900

    [Step8] Feat : 선택한 이미지가 이미지 뷰에 나타남

--- PhotoFrame/PhotoFrame/ViewController2.swift
+++ PhotoFrame/PhotoFrame/ViewController2.swift
@@ -9,8 +9,16 @@ import Foundation
 import UIKit
 
 class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.nextButtonTouched(self.nextButton)
+        picker.delegate = self
+    }
+    let picker = UIImagePickerController()
+
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var photoLabel: UILabel!
+    
     @IBAction func nextButtonTouched(_ sender: Any) {
         let random_number: Int = Int.random(in: 1...22)
         if random_number < 10 {
@@ -19,12 +27,52 @@ class ViewController2: UIViewController {
             self.photoImageView.image = UIImage(named: "\(random_number).jpg")
         }
     }
+    
+    func openLibrary(){
+      picker.sourceType = .photoLibrary
+      present(picker, animated: false, completion: nil)
+    }
+
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+        }
+        else{
+            print("Camera not available")
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.photoImageView.image = image
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+                           
+    }
     @IBAction func selectButtonTouched(_ sender: Any) {
-        
+        let alert =  UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+        self.openCamera()
+        }
+
+
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+
     }
     @IBOutlet weak var photoImageView: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.nextButtonTouched(self.nextButton)
-    }
+
+}
+
+
+
+extension ViewController2 : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
 }
