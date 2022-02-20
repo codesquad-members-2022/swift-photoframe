9d5baf695c4fd231c10ec000b58ac11ddfb5581c
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-20 14:49:23 +0900

    [ADD] imagePickerController

--- PhotoFrame/PhotoFrame/Views/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/Views/SecondViewController.swift
@@ -11,7 +11,8 @@ class SecondViewController: UIViewController {
 
     @IBOutlet weak var photoImageView: UIImageView!
     var imageNum = 1
-    
+    private let imagePickerController = UIImagePickerController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.photoImageView.image = UIImage(named: "\(String(format: "%02d", imageNum)).jpg")
@@ -22,5 +23,16 @@ class SecondViewController: UIViewController {
         self.photoImageView.image = UIImage(named: "\(String(format: "%02d", imageNum+1)).jpg")
         self.photoImageView.contentMode = .scaleAspectFill
         imageNum += 1
+    }
+    
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        //카메라 사용가능한지 체크
+        guard UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) else { return }
+        imagePickerController.sourceType = .savedPhotosAlbum
+            
+            //imagePickerController.allowsEditing = true // 촬영 후 편집할 수 있는 부분이 나온다.
+            
+        present(imagePickerController, animated: true, completion: nil)
+        
     }
 }
