ed3fcaa1e5150d8add1573e416ef169b4e6593d2
sanghee <anshee97@naver.com>
2022-02-21 01:37:14 +0900

    Feat: Add SecondVC and connect class with SecondScene

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Selina on 2022/02/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
