7480b2ee0e323267e1970d570686de98aadc220e
Jinsujin <sylpid003@gmail.com>
2022-02-20 20:58:10 +0900

    feat: PurpleViewController 생성, 스토리보드와 연결

---
+++ PhotoFrame/PhotoFrame/PurpleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PurpleViewController.swift
+//  PhotoFrame
+//
+//  Created by jsj on 2022/02/20.
+//
+
+import UIKit
+
+class PurpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
