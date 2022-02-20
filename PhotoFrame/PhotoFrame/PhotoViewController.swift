ce39481ef58c7d55e9419622ef70ae7b85a0f6c8
asqw887 <asqw887@gmail.com>
2022-02-21 00:27:58 +0900

    [add] 이미지 파일 추가

--- PhotoFrame/PhotoFrame/PhotoViewController.swift
+++ PhotoFrame/PhotoFrame/PhotoViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class PhotoViewController: UIViewController {
 
+    @IBOutlet var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +17,8 @@ class PhotoViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
     }
-    */
+    
 
 }
