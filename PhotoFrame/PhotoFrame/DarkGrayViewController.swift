11508a8ddd6f039765417fe48cb98d8eaebb2929
bibi <bibi6666667@kakao.com>
2022-02-21 11:46:56 +0900

    뷰 컨트롤러 콜백 함수의 코드 수정

--- PhotoFrame/PhotoFrame/DarkGrayViewController.swift
+++ PhotoFrame/PhotoFrame/DarkGrayViewController.swift
@@ -14,18 +14,22 @@ class DarkGrayViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
         print(#file, #line, #function, #column)
     }
         
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
         print(#file, #line, #function, #column)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
         print(#file, #line, #function, #column)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
         print(#file, #line, #function, #column)
     }
     
