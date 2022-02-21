14677e937522f4a7fb881a631afb480ad70e4bcc
LeeJoobang <hii5074@gmail.com>
2022-02-21 18:53:49 +0900

    [Add] 이미지 파일 - photoImageView 반영

--- PhotFrame/PhotFrame/SecondViewController.swift
+++ PhotFrame/PhotFrame/SecondViewController.swift
@@ -7,16 +7,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+      
     @IBOutlet weak var photoImageView: UIImageView!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    @IBAction func nextButtonTouched(_ sender: Any) {
-        
+
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+        let photoRandomNumber = arc4random_uniform(22)+1
+        let tempNumber = String(format: "%02d", photoRandomNumber)+".jpg"
+        self.photoImageView.image = UIImage(named: tempNumber)
     }
-    
+
 }
