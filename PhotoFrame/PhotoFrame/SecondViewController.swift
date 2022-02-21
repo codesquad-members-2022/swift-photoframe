cdabce2f64663a38445abbe1f7559fef34e7a46d
bibi <bibi6666667@kakao.com>
2022-02-21 13:59:27 +0900

    SecondViewController 생성 및 Outlet, Action 연결

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Bibi on 2022/02/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func nextImageButtonTouched(_ sender: Any) {
+    }
+    
+}
