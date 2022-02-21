6b070752f9f66fc35d0d79036b261bba8d81a57c
LeeJoobang <hii5074@gmail.com>
2022-02-21 11:05:32 +0900

    [Create] Second Frame - Class 초안 생성

---
+++ PhotFrame/PhotFrame/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  PhotFrame
+//
+//  Created by Joobang Lee on 2022/02/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
