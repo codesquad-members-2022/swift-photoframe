1d83b8fa761fd5be7f3fb288cf284f59563a1dec
Lee SeEun <sally4405@naver.com>
2022-02-21 11:20:28 +0900

    [step8] feat: UIImagePicker를 사용하여 사용자의 앨범에서 사진을 선택하는 기능 추가

--- PhotoFrame/PhotoFrame/YellowViewController.swift
+++ PhotoFrame/PhotoFrame/YellowViewController.swift
@@ -2,16 +2,34 @@ import UIKit
 
 class YellowViewController: UIViewController {
 
+    let imagePicker = UIImagePickerController()
     @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.photoImageView.image = UIImage(named: "01.jpg")
+        imagePicker.delegate = self
     }
     
     @IBAction func nextButtonTouched(_ sender: Any) {
         let num = Int.random(in: 1..<22)
         self.photoImageView.image = UIImage(named: "\(num > 10 ? "" : "0")\(num).jpg")
+    }
+    
+    @IBAction func selectButtonTouched(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+
+}
+
+extension YellowViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.photoImageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
     
 }
