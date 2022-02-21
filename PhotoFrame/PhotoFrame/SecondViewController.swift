b7a28c6551656429d67a169078e9872a3385c0af
sanghee <anshee97@naver.com>
2022-02-21 11:49:32 +0900

    Feat: Add selectButton and connect it with action

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -22,6 +22,9 @@ class SecondViewController: UIViewController {
     }
     
     
+    @IBAction func selectButtonTouched(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
