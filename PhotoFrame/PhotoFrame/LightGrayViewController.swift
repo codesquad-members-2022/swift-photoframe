b1ba6784f26b9df031a496c25c05339f60703ca9
bibi <bibi6666667@kakao.com>
2022-02-21 11:30:32 +0900

    불필요한 주석 삭제

--- PhotoFrame/PhotoFrame/LightGrayViewController.swift
+++ PhotoFrame/PhotoFrame/LightGrayViewController.swift
@@ -11,8 +11,6 @@ class LightGrayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,15 +32,5 @@ class LightGrayViewController: UIViewController {
     @IBAction func closeButtonTouched(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
