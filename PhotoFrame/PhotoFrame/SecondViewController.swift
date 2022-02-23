ffa773fcc0bad6880ff830de0c0b2e6cbe9b47a4
Jason <ggj00125@gmail.com>
2022-02-24 03:08:31 +0900

    FEAT: Second Scene을 관리할 swift 파일 추가 후 custom class 연결

---
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  PhotoFrame
+//
+//  Created by Jason on 2022/02/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
