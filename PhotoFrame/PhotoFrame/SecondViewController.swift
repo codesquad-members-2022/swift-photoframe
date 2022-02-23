7b3ed7583168e0a74591a5e9348f2eb5b6aeeaff
Jason <ggj00125@gmail.com>
2022-02-24 03:11:04 +0900

    FEAT: ImageView를 추가한 후 photoImageView IBOutlet으로 연결

--- PhotoFrame/PhotoFrame/SecondViewController.swift
+++ PhotoFrame/PhotoFrame/SecondViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
