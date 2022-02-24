44840c0aa64be244bf06976d8a1d3b2cf63ae93b
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-02-24 15:16:29 +0900

    Practice UIImagePickerController
    UIImagePickerController를 이용하여 사진 앱에 접근한 뒤
    사진을 불러오는 실습을 진행하였습니다.

--- PhotoFrame/PhotoFrame/SecondSceneViewController.swift
+++ PhotoFrame/PhotoFrame/SecondSceneViewController.swift
@@ -9,13 +9,33 @@ import UIKit
 
 class SecondSceneViewController: UIViewController {
     
+    let imagePicker = UIImagePickerController()
+    
     @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
     }
     
     @IBAction func nextImageButtonTouched(_ sender: UIButton) {
         self.photoImageView.image = UIImage(named: "01.jpg")
     }
+    
+    @IBAction func selectButtonTouched(_ sender: UIButton) {
+//        self.show(imagePicker, sender: nil)
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+extension SecondSceneViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.photoImageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SecondSceneViewController: UINavigationControllerDelegate {
+    
 }
